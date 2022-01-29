e24281383a6d22921afeb7fc71bc4dc25a39bac8
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-01-29 19:47:53 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,36 @@ module.exports = {
     filename: "bundle.js",
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", ".scss"],
     alias: {
       "@": path.join(__dirname, "src"),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            laoder: "style-loader",
+          },
+          {
+            laoder: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            laoder: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: "./public",
