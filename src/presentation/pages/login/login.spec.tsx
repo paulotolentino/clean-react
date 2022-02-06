3428d19d304d7478a1a11c07b8f83ca9dced58f7
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-02-06 14:35:29 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -69,6 +69,20 @@ describe("Login Component", () => {
     expect(passwordStatus.textContent).toBe("🔴");
   });
 
+  test("Should show valid email state if Validation success", () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId("email");
+
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    });
+
+    const emailStatus = sut.getByTestId("email-status");
+    expect(emailStatus.title).toBe("Tudo certo");
+    expect(emailStatus.textContent).toBe("🟢");
+  });
+
   test("Should show valid password state if Validation success", () => {
     const { sut, validationStub } = makeSut();
     validationStub.errorMessage = null;
