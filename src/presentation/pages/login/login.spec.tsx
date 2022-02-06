da92d151eef606543debc297bced12905536a7a4
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-02-06 14:56:32 -0300

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -96,4 +96,22 @@ describe("Login Component", () => {
     expect(passwordStatus.title).toBe("Tudo certo");
     expect(passwordStatus.textContent).toBe("🟢");
   });
+
+  test("Should enable submit button if form is valid", () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId("email");
+    const passwordInput = sut.getByTestId("password");
+
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    });
+
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(false);
+  });
 });
