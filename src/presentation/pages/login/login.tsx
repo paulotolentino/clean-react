b4b459a4d01bfc78f4d5f6230cb24d852c821015
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-02-10 18:27:44 -0300

    feat: ensure Login present error and ride spinner on Authentication failure

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -37,12 +37,23 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
-    if (state.isLoading || state.emailError || state.passwordError) return;
-    setState({
-      ...state,
-      isLoading: true,
-    });
-    await authentication.auth({ email: state.email, password: state.password });
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) return;
+      setState({
+        ...state,
+        isLoading: true,
+      });
+      await authentication.auth({
+        email: state.email,
+        password: state.password,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: error.message,
+      });
+    }
   };
 
   return (
