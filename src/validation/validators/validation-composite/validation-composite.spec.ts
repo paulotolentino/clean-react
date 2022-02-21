beb191cd1340ce6201e3dcbf8ecd0f01a84d348e
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-02-20 21:06:52 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,16 @@
+import { FieldValidationSpy } from "../test/mock-field-validation";
+import { ValidationComposite } from "./validation-composite";
+
+describe("ValidationComposite", () => {
+  test("Should return error if any validation fails", () => {
+    const fieldvalidationSpy = new FieldValidationSpy("any_field");
+    const fieldvalidationSpy2 = new FieldValidationSpy("any_field");
+    fieldvalidationSpy2.error = new Error("any_error_message");
+    const sut = new ValidationComposite([
+      fieldvalidationSpy,
+      fieldvalidationSpy2,
+    ]);
+    const error = sut.validate("any_field", "any_value");
+    expect(error).toBe("any_error_message");
+  });
+});
