904484acd492ffdb544f79b4473fe925355b91c6
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2022-03-20 18:36:29 -0300

    fix: change webpack env var to use JSON.stringify

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
-const path = require("path");
-const { DefinePlugin } = require("webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const { DefinePlugin } = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main/index.tsx",
+  mode: 'development',
+  entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, "public/js"),
-    publicPath: "/public/js",
-    filename: "bundle.js",
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".scss"],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
-      "@": path.join(__dirname, "src"),
-    },
+      '@': path.join(__dirname, 'src')
+    }
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              modules: true,
-            },
+              modules: true
+            }
           },
           {
-            loader: "sass-loader",
-          },
-        ],
-      },
-    ],
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     devMiddleware: {
-      writeToDisk: true,
+      writeToDisk: true
     },
     static: {
-      directory: "./public",
+      directory: './public'
     },
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   externals: {
-    react: "React",
-    "react-dom": "ReactDOM",
+    react: 'React',
+    'react-dom': 'ReactDOM'
   },
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      "process.env.API_URL": "http://fordevs.herokuapp.com/api",
-    }),
-  ],
-};
+      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+    })
+  ]
+}
